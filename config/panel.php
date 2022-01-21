839708dada990e7e88d4d430005959fab7e4da60
DreamWillBeReal <120799088+DarkHorseCorder@users.noreply.github.com>
2022-01-21 11:35:55 -0800

    config folder

---
+++ config/panel.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'date_format'         => 'Y-d-m',
+    'time_format'         => 'H:i:s',
+    'primary_language'    => 'en',
+    'available_languages' => [
+        'en' => 'English',
+    ],
+];
---
+++ config/translatable.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'locales' => [
+        'en',
+        'ar',
+        'de',
+    ],
+
+    /*
+     * If a translation has not been set for a given locale, use this locale instead.
+     */
+    'fallback_locale' => env('FALLBACK_LANGUAGE'),
+];
